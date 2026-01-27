de6ec83ff09ec9b34d65fb2bed627edff47ee72b
Luan-Felipe-R-Sanches <luanfeliperodrigues25@gmail.com>
2026-01-27 12:26:26 -0400

    Criando schemas

--- car_api/car_api/routes/users.py
+++ car_api/car_api/routes/users.py
@@ -5,5 +5,5 @@ router = APIRouter()
 @router.get("/", status_code=status.HTTP_200_OK)
 async def list_users():
     return {"users": [
-            {"id": 1, "email": "luan@gmail.com"},
+            {"id": 1, "username": "luan", "email": "luan@gmail.com"},
     ]}
