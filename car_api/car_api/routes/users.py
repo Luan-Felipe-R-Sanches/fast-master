49b46a7e13d5c9f543b438ddad15e880eac3e276
Luan-Felipe-R-Sanches <luanfeliperodrigues25@gmail.com>
2026-01-28 12:55:56 -0400

    Criando usuarios

--- car_api/car_api/routes/users.py
+++ car_api/car_api/routes/users.py
@@ -1,8 +1,22 @@
 from fastapi import APIRouter, status
-from car_api.schemas.users import UserListPublicSchema
 
+from car_api.schemas.users import (
+    UserScreen,
+    UserListPublicSchema,
+    UserPublicSchema,
+
+)       
 
 router = APIRouter()
+
+@router.post(path="/",
+             status_code=status.HTTP_201_CREATED,
+             response_model=UserPublicSchema,)
+async def create_user(user:UserScreen):
+    user_with_id = UserPublicSchema(**user.model_dump(), id=len(USERS)+1)
+    USERS.append(user_with_id)
+    return user_with_id
+
 
 @router.get("/",
             status_code=status.HTTP_200_OK,
