db86e8f335c9f38a88e02e70ca50aa3657686025
Luan-Felipe-R-Sanches <luanfeliperodrigues25@gmail.com>
2026-01-28 12:28:25 -0400

    Configurando uso de schemas

--- car_api/car_api/routes/users.py
+++ car_api/car_api/routes/users.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter, status
+from car_api.schemas.users import UserListPublicSchema
+
 
 router = APIRouter()
 
-@router.get("/", status_code=status.HTTP_200_OK)
+@router.get("/",
+            status_code=status.HTTP_200_OK,
+            response_model=UserListPublicSchema,)
 async def list_users():
     return {"users": [
             {"id": 1, "username": "luan", "email": "luan@gmail.com"},
