eb4b8c720f7248f94f6ef40d6fa9f77dfe0d72e3
Luan-Felipe-R-Sanches <luanfeliperodrigues25@gmail.com>
2026-01-30 12:48:35 -0400

    Conexão e sessão do banco de dados

---
+++ car_api/car_api/core/database.py
@@ -0,0 +1,8 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from car_api.core.settings import Settings
+
+engine = create_async_engine(Settings().DATABASE_URL)
+
+async def get_session():
+     async with AsyncSession(engine, exceptions_on_commit=False) as session:
+           yield session
